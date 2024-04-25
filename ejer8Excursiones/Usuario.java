e20051997bd8e8b17dc26f308c820b3bf8bec77d
TeoBarrena <127796721+TeoBarrena@users.noreply.github.com>
2024-04-25 17:51:51 -0300

    Create Usuario.java

---
+++ ejer8Excursiones/Usuario.java
@@ -0,0 +1,15 @@
+package oo1.ej9Excursiones002;
+
+public class Usuario {
+	private String nombre;
+	private String mail;
+	
+	public Usuario(String nombre, String mail) {
+		this.nombre = nombre; 
+		this.mail = mail;
+	}
+	
+	public String getMail() {
+		return this.mail;
+	}
+}
